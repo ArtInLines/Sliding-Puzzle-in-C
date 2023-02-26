a7978b434867532b915998ed2e3e0d3534f70590
ArtInLines <linesinart@gmail.com>
2023-02-26 23:51:37 +0100

    Show output in PerfTest before board's created

--- perftest.c
+++ perftest.c
@@ -22,48 +22,49 @@ double create_initial_board_test(int column_size, int row_size, int variance) {
 double A_star_test(int column_size, int row_size, int bias, int variance) {
     int *empty_field, *board, *path;
     double begin_time, end_time;
-    
+
     board = create_initial_board(column_size, row_size, variance);
     empty_field = get_empty_field(column_size, board);
-    
+
     begin_time = get_time();
     path = A_star(column_size, row_size, bias, empty_field, board, 0);
     end_time = get_time();
     free(path);
-    
+
     return end_time - begin_time;
 }
 
 void run_test(int column_size, int row_size, int bias, int variance) {
     double create_board_time, a_star_time;
-    
+
+    printf("\rsize=%ix%i, variance=%i, bias=%i, ", column_size, row_size, variance, bias);
+
     create_board_time = create_initial_board_test(column_size, row_size, variance);
-    
-    printf("\rsize=%ix%i, variance=%i, bias=%i, create_board_time=%.4fs, ", column_size, row_size, variance, bias, create_board_time);
-    
+    printf("create_board_time=%.4fs, ", create_board_time);
+
     a_star_time = A_star_test(column_size, row_size, bias, variance);
-    printf("solve_time=%3.3fs", a_star_time);
-    
+    printf("solve_time=%3.4fs", a_star_time);
+
 }
 
 void main() {
     int column_size, row_size, bias, variance;
     clear_screen();
-    
+
     while (1) {
         printf("Column Size? ");
         scanf("%d", &column_size);
-        
+
         printf("Row Size? ");
         scanf("%d", &row_size);
-        
+
         printf("Bias? ");
         scanf("%d", &bias);
-        
+
         printf("Variance (negative => Max. Integer)? ");
         scanf("%d", &variance);
         if (variance < 0) variance = INT_MAX;
-        
+
         run_test(column_size, row_size, bias, variance);
         printf("\n\n");
     }
